11a101427b6b1c6126ead5b23babf293ab5d5239
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-01-28 16:54:46 -0500

    added TNG characters

--- clicky-game-react/src/App.js
+++ clicky-game-react/src/App.js
@@ -1,52 +1,8 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
-import "./App.css";
-import Counter from "./Counter";
-class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    friends
-  };
-  
-  
+import React from "react";
+import Game from "./components/Game";
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
-
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-            
-          />
-        ))}
-        <Counter />
-      </Wrapper>
-      
-    );
-  }
+function App() {
+  return <Game />;
 }
 
 export default App;
